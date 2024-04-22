fb109cde34f12f134d51b8bc6d1e320a3ce2edb3
Raghad-ali-Alotaibi <raghad.ali.alotaibi@gmail.com>
2024-04-22 21:37:01 +0300

    TABLE Customer

--- Database.sql
+++ Database.sql
@@ -1,3 +1,4 @@
+-- Category
 CREATE TABLE Category(
 CategoryID SERIAL PRIMARY KEY,
 CategoryName VARCHAR(50) UNIQUE NOT NULL
@@ -21,3 +22,31 @@ OR CategoryName = 'Home & Kitchen';
 UPDATE Category 
 SET CategoryName = 'Beauty Products' 
 WHERE CategoryID = 2;
+
+-- Customer
+CREATE TABLE Customer( 
+CustomerID SERIAL PRIMARY KEY, 
+CustomerFirstName VARCHAR(50) NOT NULL, 
+CustomerLastName VARCHAR(50) NOT NULL,
+CustomerEmail VARCHAR(100) UNIQUE NOT NULL, 
+CustomerPassword VARCHAR(100) NOT NULL,  
+CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO Customer (CustomerFirstName, CustomerLastName, CustomerEmail, CustomerPassword)
+VALUES
+    ('Raghad', 'Alotaibi', 'Raghad@gmail.com', '11112'),
+    ('Somayah', 'Absi', 'somayah@gmail.com', '222221'),
+    ('Nada', 'Yhaya', 'Nada@gmail.com', '333311'),
+    ('Albandri', 'Alotaibi', 'Albandri@gmail.com', '11442');
+
+
+SELECT * FROM Customer; 
+
+SELECT CustomerID, CustomerFirstName, CustomerLastName FROM Customer;
+
+SELECT * FROM Customer WHERE CustomerID = 1;
+
+UPDATE Customer SET CustomerFirstName='sadeem' WHERE CustomerID = 2;
+
+DELETE FROM Customer WHERE CustomerID = 1;
