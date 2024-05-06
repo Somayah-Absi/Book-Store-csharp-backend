2f3a3f9d2185f9d2f9f414c88c9d1c1f473d1bc1
nada <nadayahya2020@hotmail.com>
2024-05-05 18:11:12 -0700

    Done LoginDto & RegisterDto

---
+++ Dtos/RegisterDto.cs
@@ -0,0 +1,11 @@
+namespace auth.Dtos
+{
+    public class RegisterDto
+    {
+        public string? FirstName { set; get; }
+        public string? LastName { set; get; }
+        public string? Email { set; get; }
+        public string? Password { set; get; }
+        public string? Mobile {set; get;}
+    }
+}
