9d36ec87dc770ee8a5cd728e8c316c2cc6f5c908
nada <nadayahya2020@hotmail.com>
2024-05-05 17:58:40 -0700

    Done Jwt token class ,keyGenerate

---
+++ Helpers/JwtService.cs
@@ -0,0 +1,44 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace auth.Helpers
+{
+    public class JwtService
+    {
+
+        public static string ConvertKeyToString(byte[] keyBytes)
+        {
+            return Convert.ToBase64String(keyBytes);
+        }
+        // Generate a 32-byte (256-bit) random key
+        private string secureKey = KeyGenerator.GenerateRandomKey(32);
+
+        public string Generate(int id)
+        {
+            var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secureKey));
+            var credentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256Signature);
+            var header = new JwtHeader(credentials);
+
+            var payload = new JwtPayload(id.ToString(), null, null, null, DateTime.Today.AddDays(1)); // 1 day
+            var securityToken = new JwtSecurityToken(header, payload);
+
+            return new JwtSecurityTokenHandler().WriteToken(securityToken);
+        }
+
+        public JwtSecurityToken Verify(string jwt)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(secureKey);
+            tokenHandler.ValidateToken(jwt, new TokenValidationParameters
+            {
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuerSigningKey = true,
+                ValidateIssuer = false,
+                ValidateAudience = false
+            }, out SecurityToken validatedToken);
+
+            return (JwtSecurityToken)validatedToken;
+        }
+    }
+}
