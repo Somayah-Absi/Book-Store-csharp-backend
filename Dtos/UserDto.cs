5b9c5651b9a3e28edeb92bb846c6aa5351955f83
nada <nadayahya2020@hotmail.com>
2024-05-09 05:11:18 -0700

    Add some attribute to UserDto

--- Dtos/UserDto.cs
+++ Dtos/UserDto.cs
@@ -4,10 +4,13 @@ namespace Backend.Dtos
 {
     public class UserDto
     {
-        public int UserId { get; set; }
-        public string FirstName { get; set; } = null!;
-        public string LastName { get; set; } = null!;
-        public string Email { get; set; } = null!;
-        public string? Mobile { get; set; } = null!;
+    public int UserId { get; set; }
+    public string FirstName { get; set; } =  string.Empty;
+    public string LastName { get; set; } =  string.Empty;
+    public required string Email { get; set; } = null!;
+    public required string? Mobile { get; set; }
+    public DateTime? CreatedAt { get; set; }
+    public bool? IsAdmin { get; set; } = false;
+    public bool? IsBanned { get; set; } = false;
     }
 }
