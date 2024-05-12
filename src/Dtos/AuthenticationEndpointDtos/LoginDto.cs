afe8ce7aac84e64de51b3b3787243a5452f51d59
nada <nadayahya2020@hotmail.com>
2024-05-11 17:01:56 -0700

    Done Add Documentation to code

--- src/Dtos/AuthenticationEndpointDtos/LoginDto.cs
+++ src/Dtos/AuthenticationEndpointDtos/LoginDto.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace auth.Dtos
 {
     public class LoginDto
     {
+        [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", ErrorMessage = "Invalid email address")]
         public string? Email { set; get; }
         public string? Password { set; get; }
 
--- src/Services/JwtService.cs
+++ src/Services/JwtService.cs
@@ -31,29 +31,43 @@ namespace auth.Helpers
 
             // Convert the key to bytes
             var key = Encoding.ASCII.GetBytes(jwtKey);
+
             // Check if the key is null
             if (key == null || key.Length == 0)
             {
                 throw new InvalidOperationException("JWT key is not configured properly.");
             }
+
+            // Determine if the user is an admin
             bool isAdmin = user.IsAdmin ?? false;
 
+            // Create a token descriptor specifying the claims, expiration, and signing credentials
             var tokenDescriptor = new SecurityTokenDescriptor
             {
+                // Define the claims for the token
                 Subject = new ClaimsIdentity(new[] {
-                new Claim(ClaimTypes.NameIdentifier, user.UserId.ToString()),
-                new Claim(ClaimTypes.Role, isAdmin? "Admin" : "User"),
-            }),
+            new Claim(ClaimTypes.NameIdentifier, user.UserId.ToString()), // User ID
+            new Claim(ClaimTypes.Role, isAdmin? "Admin" : "User"), // Role (Admin/User)
+        }),
 
+                // Set the expiration time for the token (e.g., 2 minutes from now)
                 Expires = DateTime.UtcNow.AddMinutes(2),
+
+                // Specify the signing credentials using the JWT key
                 SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
+
+                // Set the issuer and audience of the token
                 Issuer = _configuration["Jwt:Issuer"],
                 Audience = _configuration["Jwt:Audience"],
             };
 
+            // Create a JWT token handler
             var tokenHandler = new JwtSecurityTokenHandler();
+
+            // Generate the JWT token
             var token = tokenHandler.CreateToken(tokenDescriptor);
 
+            // Write the JWT token as a string
             return tokenHandler.WriteToken(token);
         }
     }
